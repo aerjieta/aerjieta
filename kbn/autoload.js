8d6a2ffa752428a852b51f31b6b367e3b496ce33
aerjieta <66709571+aerjieta@users.noreply.github.com>
2024-04-24 21:01:44 +0800

    Update autoload.js

--- kbn/autoload.js
+++ kbn/autoload.js
@@ -1,8 +1,5 @@
-// live2d_path 参数建议使用绝对路径
 const live2d_path = "https://aerjieta.vercel.app/kbn/";
-//const live2d_path = "/live2d-widget/";
 
-// 封装异步加载资源的方法
 function loadExternalResource(url, type) {
 	return new Promise((resolve, reject) => {
 		let tag;
@@ -24,14 +21,12 @@ function loadExternalResource(url, type) {
 	});
 }
 
-// 加载 waifu.css live2d.min.js waifu-tips.js
-if (screen.width >= 100) {
+if (screen.width >= 700) {
 	Promise.all([
 		loadExternalResource(live2d_path + "waifu.css", "css"),
 		loadExternalResource(live2d_path + "live2d.min.js", "js"),
 		loadExternalResource(live2d_path + "waifu-tips.js", "js")
 	]).then(() => {
-		// 配置选项的具体用法见 README.md
 		initWidget({
 			waifuPath: live2d_path + "waifu-tips.json",
 			//apiPath: "https://live2d.fghrsh.net/api/",
